1aff04c76a27d470ccf5805d2dc160018cebf478
Dmitrijs Zaicevs <A230223DZ@rvt.lv>
2026-01-27 03:37:59 +0200

    .

--- Frontend/ApgerbjuVeikalsExam/src/views/ShopView.vue
+++ Frontend/ApgerbjuVeikalsExam/src/views/ShopView.vue
@@ -78,12 +78,12 @@ type Product = {
 
 // Данные
 const products = ref<Product[]>([
-  { id: 1, name: '1', price: 29, image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Футболки' },
-  { id: 2, name: '2', price: 89, image: 'https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Джинсы' },
-  { id: 3, name: '3', price: 65, image: 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Худи' },
-  { id: 4, name: '4', price: 120, image: 'https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Куртки' },
-  { id: 5, name: '5', price: 55, image: 'https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Рубашки' },
-  { id: 6, name: '6', price: 25, image: 'https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Футболки' },
+  { id: 1, name: '1', price: 29, image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Tshirt' },
+  { id: 2, name: '2', price: 89, image: 'https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Jeans' },
+  { id: 3, name: '3', price: 65, image: 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Hoodie' },
+  { id: 4, name: '4', price: 120, image: 'https://images.unsplash.com/photo-1551028719-00167b16eac5?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Jacket' },
+  { id: 5, name: '5', price: 55, image: 'https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Shirt' },
+  { id: 6, name: '6', price: 25, image: 'https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', category: 'Tshirt' },
 ])
 
 const categories = ref([
